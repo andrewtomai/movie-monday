fe26640ae5d3b706321891d9e9fe7f6ece9b94d9
Andrew Tomai <tomaiandrew@gmail.com>
2026-06-01 23:22:17 -0700

    reorg

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [tailwindcss(), react(), cloudflare()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src/ui"),
     },
   },
 });
